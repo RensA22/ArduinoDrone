bcc5c197e3fc14a476aa69e6ff0ed4c896868ef7
Rens Aerts <rens.aerts@ziggo.nl>
2022-02-18 11:26:41 +0100

    trying to get the sensor data right

--- lib/MPU6050/MPU6050.cpp
+++ lib/MPU6050/MPU6050.cpp
@@ -10,10 +10,11 @@
 #include "math.h"
 
 MPU6050::MPU6050() :
-		wire(&Wire), address(0x68), prevMicros(micros()), updateHz(0), offXgyro(
-				0), offYgyro(0), offZgyro(0), offXacc(0), offYacc(0), offZacc(
-				0), angleX(0.0), angleY(0.0), angleZ(0.0), first_run(true) {
-	setup();
+		wire(&Wire), address(0x68), prevMicros(micros()), updateHz(0), gyro( {
+				.sensitivity = 65.5 }), accel( { .sensitivity = 8192 }), roll(
+				0.0), pitch(0.0), yaw(0.0), rollMotion(0.0), pitchMotion(0.0), first_run(
+				true) {
+
 }
 
 MPU6050::~MPU6050() {
@@ -26,92 +27,176 @@ MPU6050& MPU6050::getMPU6050Instance() {
 }
 
 void MPU6050::setup() {
-	if (!begin()) {
+	while (!begin()) {
 		Serial.println("Could not connect to GY521");
+		delay(500);
 	}
 
+	//low pass filter
+	setRegister(0x1a, 0b000000101);
+
 	//setGyroSensitivity 65.5
-	setRegister(0x1b, 0b00000000);
+	setRegister(0x1b, 0b00001000);
 	//setAccelSensitivity 8192
-	setRegister(0x1c, 0b00000000);
+	setRegister(0x1c, 0b00001000);
 
-	setRegister(0x1a, 0b00000011);
+//	setRegister(0x1a, 0b00000011);
+//
+//	setRegister(0x19, 0b00000100);
 
-	setRegister(0x19, 0b00000100);
+	calculateOffset();
+}
 
-	offXgyro = -378;
-	offYgyro = 11;
-	offZgyro = 103;
+void MPU6050::calculateOffset() {
+	uint32_t nLoops = 2000;
+	Serial.println("Calculate offset?");
+	int64_t offX = 0;
+	int64_t offY = 0;
+	int64_t offZ = 0;
+	for (uint32_t i = 0; i < nLoops; i++) {
+		int16_t rawXgyro = getRegister(0x43) << 8 | getRegister(0x44);
+		int16_t rawYgyro = getRegister(0x45) << 8 | getRegister(0x46);
+		int16_t rawZgyro = getRegister(0x47) << 8 | getRegister(0x48);
 
-	offXacc = -126;
-	offYacc = 360;
-	offZacc = 17257;
+		offX += rawXgyro;
+		offY += rawYgyro;
+		offZ += rawZgyro;
+		yield();
+	}
+
+	gyro.offsetX = round(offX / nLoops) * -1;
+	gyro.offsetY = round(offY / nLoops) * -1;
+	gyro.offsetZ = round(offZ / nLoops) * -1;
+
+	Serial.print("XgyroOffset: ");
+	Serial.print(gyro.offsetX);
+	Serial.print(", YgyroOffset: ");
+	Serial.print(gyro.offsetY);
+	Serial.print(", ZgyroOffset: ");
+	Serial.println(gyro.offsetZ);
+
 }
 
 void MPU6050::update() {
+	updateGyroData();
+	updateAccData();
 
+//	Serial.print("BeforeAngleX:\t");
+//	Serial.print(gyro.angleX);
+
+//	if (!first_run) {
+//	gyro.angleX = (0.9996 * gyro.angleX) + (0.0004 * accel.angleX);
+//	gyro.angleY = (0.9996 * gyro.angleY) + (0.0004 * accel.angleY);
+//	} else {
+//		gyro.angleX = accel.angleX;
+//		gyro.angleY = accel.angleY;
+//
+//		first_run = false;
+//	}
+
+	roll = roll * 0.9 + gyro.angleX * 0.1;
+	pitch = pitch * 0.9 + gyro.angleY * 0.1;
+
+	rollMotion = rollMotion * 0.7 + 0.3 * gyro.rawX / gyro.sensitivity;
+	pitchMotion = pitchMotion * 0.7 + 0.3 * gyro.rawY / gyro.sensitivity;
+
+//	Serial.print("\tAngleX:\t");
+//	Serial.print(gyro.angleX);
+//	Serial.print("\tangleY:\t");
+//	Serial.print(gyro.angleY);
+//	Serial.print("\taccX:\t");
+//	Serial.print(accel.angleX);
+//	Serial.print("\taccY:\t");
+//	Serial.print(accel.angleY);
+//	Serial.print("\troll:\t");
+//	Serial.print(roll);
+//	Serial.print("\tpitch:\t");
+//	Serial.println(pitch);
+
+}
+
+void MPU6050::updateGyroData() {
 	//Get raw gyro values from the mpu6050 register
-	int16_t rawXgyro = getRegister(0x43) << 8 | getRegister(0x44);
-	int16_t rawYgyro = getRegister(0x45) << 8 | getRegister(0x46);
-	int16_t rawZgyro = getRegister(0x47) << 8 | getRegister(0x48);
+	gyro.rawX = getRegister(0x43) << 8 | getRegister(0x44);
+	gyro.rawY = getRegister(0x45) << 8 | getRegister(0x46);
+	gyro.rawZ = getRegister(0x47) << 8 | getRegister(0x48);
 
 	//add offsets to it;
-	rawXgyro += offXgyro;
-	rawYgyro += offYgyro;
-	rawZgyro += offZgyro;
+	gyro.rawX += round(gyro.offsetX);
+	gyro.rawY += round(gyro.offsetY);
+	gyro.rawZ += round(gyro.offsetZ);
+	float angX = 0.0;
+	float angY = 0.0;
+	if (updateHz < 2) {
 
-	//Calculate the raw data to degrees per sec
-	angleX += rawXgyro * (1.0 / 131.0) * updateHz;
-	angleY += rawYgyro * (1.0 / 131.0) * updateHz;
-	angleZ += rawZgyro * (1.0 / 131.0) * updateHz;
+		//Calculate the raw data to degrees per sec
+		angX = (float) -gyro.rawX / gyro.sensitivity * updateHz;
+		angY = (float) gyro.rawY / gyro.sensitivity * updateHz;
 
-	//Get the raw accelero value from the mpu6050 registers
-	int16_t rawXAcc = getRegister(0x3b) << 8 | getRegister(0x3c);
-	int16_t rawYAcc = getRegister(0x3d) << 8 | getRegister(0x3e);
-	int16_t rawZAcc = getRegister(0x3f) << 8 | getRegister(0x40);
-
-	//add offsets to it
-	rawXAcc += offXacc;
-	rawYAcc += offYacc;
-	rawZAcc += offZacc;
-
-	//caculate the G's from the raw data
-	float xAcc = rawXAcc / 16384.0;
-	float yAcc = rawYAcc / 16384.0;
-	float zAcc = rawZAcc / 16384.0;
-
-	//convert the g's to a angle WIP
-	float accelRoll = atan2(zAcc, yAcc) * (180 / PI) + 90.0;
-	float accelPitch = atan2(zAcc, xAcc) * (180 / PI) + 90.0;
-
-	if (first_run) {
-		angleX = accelRoll;
-		first_run = false;
+		if (angX > 0.01 || angX < -0.01) {
+			gyro.angleX += angX;
+		}
+		if (angY > 0.01 || angY < -0.01) {
+			gyro.angleY += angY;
+		}
 	}
 
-	float accelSum = abs(rawXAcc) + abs(rawYAcc) + abs(rawZAcc);
-	float gyroSum = abs(rawXgyro) + abs(rawYgyro) + abs(rawZgyro);
-
-	if (abs(angleX - accelRoll) > 5) {
-		angleX = accelRoll;
-	}
-	if (abs(angleY - accelPitch) > 5) {
-		angleY = accelPitch;
-	}
-
-	if (accelSum > 0.1 && accelSum < 3 && gyroSum > 0.3) { //Check that th movement is not very high (therefore providing inacurate angles)
-		angleX = angleX * 0.98 + 0.02 * accelRoll;
-	} else if (gyroSum > 0.3) { //Use the gyroscope angle if the acceleration is high
-
-	} else if (gyroSum <= 0.3) { //Use accelerometer angle if not much movement
-		angleX = accelRoll;
-	}
-
-	Serial.println(accelRoll);
+//	gyro.angleZ += zGryo / gyro.sensitivity * updateHz;
+//
+//	Serial.print("RawGyroY"
+//			":\t");
+//	Serial.print(gyro.rawY);
+//	Serial.print("\tRawGyroY:\t");
+//	Serial.print(gyro.rawY);
+//	Serial.print("\tSens:\t");
+//	Serial.print(gyro.sensitivity, 5);
+//	Serial.print("\tupdateHz:\t");
+//	Serial.print(updateHz, 5);
+//	Serial.print("\tOutput:\t");
+//	Serial.print(angX, 5);
+//	Serial.print("\tangleY:\t");
+//	Serial.print(gyro.angleY, 5);
+//	if (angY > 0.01 || angY < -0.01) {
+//		Serial.print("********");
+//	}
 
 //Calcule the looptime in seconds. Micros are used to up the accuracy.
 	updateHz = (float) (micros() - prevMicros) / 1e6;
 	prevMicros = micros();
+}
+
+void MPU6050::updateAccData() {
+//Get the raw accelero value from the mpu6050 registers
+	int16_t rawXAcc = getRegister(0x3b) << 8 | getRegister(0x3c);
+	int16_t rawYAcc = getRegister(0x3d) << 8 | getRegister(0x3e);
+	int16_t rawZAcc = getRegister(0x3f) << 8 | getRegister(0x40);
+
+	uint64_t total_acc = sqrt(
+			pow(rawXAcc, 2) + pow(rawYAcc, 2) + pow(rawZAcc, 2));
+
+	if (abs(rawXAcc) < total_acc) {
+		accel.angleX = asin((float) rawYAcc / total_acc) * (180 / PI) * -1;
+	}
+
+	if (abs(rawYAcc) < total_acc) {
+		accel.angleY = asin((float) rawXAcc / total_acc) * (180 / PI) * -1;
+	}
+
+//add offsets to it
+//	float xAcc = rawXAcc + accel.offsetX;
+//	float yAcc = rawYAcc + accel.offsetY;
+//	float zAcc = rawZAcc + accel.offsetZ;
+
+//caculate the G's from the raw data
+//	float xAcc = rawXAcc / accel.sensitivity;
+//	float yAcc = rawYAcc / accel.sensitivity;
+//	float zAcc = rawZAcc / accel.sensitivity;
+//
+//	accel.angleX = (atan(yAcc / ((xAcc * xAcc) + (zAcc * zAcc))) * (180 / PI));
+//	accel.angleY = atan(xAcc / ((yAcc * yAcc) + (zAcc * zAcc))) * (180 / PI);
+
+//	accel.angleX = 180 / PI * (atan2(-yAcc, -zAcc) + PI) - 180;
+//	accel.angleY = 180 / PI * (atan2(-xAcc, -zAcc) + PI) - 180;
 }
 
 bool MPU6050::begin() {
@@ -135,16 +220,22 @@ bool MPU6050::isConnected() {
 }
 
 void MPU6050::reset() {
-	angleX = 0;
-	angleY = 0;
-	angleZ = 0;
+	roll = 0;
+	pitch = 0;
+	yaw = 0;
+
+	rollMotion = 0;
+	pitchMotion = 0;
+
+	gyro.reset();
+	accel.reset();
 }
 
 void MPU6050::setRegister(uint8_t reg, uint8_t value) {
 	wire->beginTransmission(address);
 	wire->write(reg);
 	wire->write(value);
-	// no need to do anything if not connected.
+// no need to do anything if not connected.
 	if (wire->endTransmission() != 0) {
 		Serial.println("Error writing to register");
 	}
@@ -154,26 +245,30 @@ uint8_t MPU6050::getRegister(uint8_t reg) {
 	wire->beginTransmission(address);
 	wire->write(reg);
 	if (wire->endTransmission() != 0) {
-		Serial.println("Error getting register");
+		Serial.println("Error getting register endTransmission !=0");
 		return 0;
 	}
 	uint8_t n = wire->requestFrom(address, (uint8_t) 1);
 	if (n != 1) {
-		Serial.println("Error getting register");
+		Serial.println("Error getting register N != 1");
 		return 0;
 	}
 	uint8_t val = wire->read();
 	return val;
 }
 
-float MPU6050::getAngleX() const {
-	return angleX;
+float MPU6050::getPitch() const {
+	return pitch;
 }
 
-float MPU6050::getAngleY() const {
-	return angleY;
+float MPU6050::getRoll() const {
+	return roll;
 }
 
-float MPU6050::getAngleZ() const {
-	return angleZ;
+float MPU6050::getPitchMotion() const {
+	return pitchMotion;
+}
+
+float MPU6050::getRollMotion() const {
+	return rollMotion;
 }
--- lib/MPU6050/MPU6050.h
+++ lib/MPU6050/MPU6050.h
@@ -10,6 +10,45 @@
 
 #include <Wire.h>
 
+struct sensor {
+	int16_t offsetX;
+	int16_t offsetY;
+	int16_t offsetZ;
+
+	float angleX;
+	float angleY;
+	float angleZ;
+
+	int16_t rawX;
+	int16_t rawY;
+	int16_t rawZ;
+
+	float sensitivity;
+
+	void reset() {
+		angleX = 0.0;
+		angleY = 0.0;
+		angleZ = 0.0;
+	}
+
+	void print() {
+		Serial.print("offsetX: ");
+		Serial.print(offsetX);
+		Serial.print(", offsetY: ");
+		Serial.print(offsetY);
+		Serial.print(", offsetZ: ");
+		Serial.println(offsetZ);
+		Serial.print("angleX: ");
+		Serial.print(angleX);
+		Serial.print(", angleY: ");
+		Serial.print(angleY);
+		Serial.print(", angleZ: ");
+		Serial.println(angleZ);
+		Serial.print("Sensitivity: ");
+		Serial.println(sensitivity);
+	}
+};
+
 class MPU6050 {
 public:
 	static MPU6050& getMPU6050Instance();
@@ -22,9 +61,11 @@ public:
 	void update();
 	void reset();
 
-	float getAngleX() const;
-	float getAngleY() const;
-	float getAngleZ() const;
+	void calculateOffset();
+	float getPitch() const;
+	float getRoll() const;
+	float getPitchMotion() const;
+	float getRollMotion() const;
 
 private:
 	MPU6050();
@@ -36,22 +77,23 @@ private:
 	void setRegister(uint8_t reg, uint8_t value);
 	uint8_t getRegister(uint8_t reg);
 
+	void updateGyroData();
+	void updateAccData();
+
 	TwoWire *wire;
 	uint8_t address;
 	uint32_t prevMicros;
 	float updateHz;
 
-	int16_t offXgyro;
-	int16_t offYgyro;
-	int16_t offZgyro;
+	sensor gyro;
+	sensor accel;
 
-	int16_t offXacc;
-	int16_t offYacc;
-	int16_t offZacc;
+	float roll;
+	float pitch;
+	float yaw;
 
-	float angleX;
-	float angleY;
-	float angleZ;
+	float rollMotion;
+	float pitchMotion;
 
 	bool first_run;
 
